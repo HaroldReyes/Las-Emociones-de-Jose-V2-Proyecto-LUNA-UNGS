70d3af7f2f75a6e7eec069c810a3ec9bb7580454
HaroldReyes <haroldthomasreyes@gmail.com>
2017-02-20 00:10:55 -0300

    Inicio

---
+++ LasEmocionesDeJoseV2/LasEmocionesDeJoseV2/Home.py
@@ -0,0 +1,24 @@
+from Escena import*
+class Home(Scene):
+    def __init__(self, director):
+        Scene.__init__(self, director)
+        
+    def on_update(self):
+        pass
+ 
+    def on_event(self):
+        pass
+ 
+    def on_draw(self, screen):
+        pass
+
+
+    
+
+# ---------------------------------------------------------------------
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
